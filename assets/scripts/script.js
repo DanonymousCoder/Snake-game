c10cf24ec438ff95a1750c336e81105962f54091
Ridwan <aladebaruwa@gmail.com>
2024-07-28 20:37:45 +0100

    Added game variables, drawFood function, generateFood function, and move function to script.js; commented out instruction text and logo in index.html

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -4,23 +4,26 @@ const board = document.getElementById('game-board');
 
 // Define game variables
 
+const gridSize = 20;
 let snake = [{x: 10, y: 10}];
+let food = generateFood();
 
 // Draw game map, snake, food
 
 function draw() {
     board.innerHTML = '';
     drawSnake();
+    drawFood();
 }
 
 // Draw Snake
 
 function drawSnake() {
     snake.forEach((segment) => {
-        const snakeElement = createGameElement('div', 'snake')
-    });
+        const snakeElement = createGameElement('div', 'snake');
     SetPosition(snakeElement, segment);
     board.appendChild(snakeElement)
+    });
 }
 
 // Create a snake or food cube/div
@@ -40,4 +43,35 @@ function SetPosition(element, position) {
 
 
 // Testing draw function
-draw();
+ draw();
+
+ // Generate Food
+
+function drawFood() {
+    const foodElement = createGameElement('div', 'food');
+    SetPosition(foodElement, food)
+    board.appendChild(foodElement);
+}
+
+ // Generate Food
+
+function generateFood() {
+    const x = Math.floor(Math.random() * gridSize) + 1;
+    const y = Math.floor(Math.random() * gridSize) + 1;
+    return {x, y};
+}
+
+// Moving the snake 
+
+function move() {
+    const head = { ...snake[0] };
+
+    switch (direction) {
+        case value:
+
+            break;
+
+        default:
+            break;
+    }
+}
