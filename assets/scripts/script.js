0341a05c525ae48ed6b6bb8ff171cd0c6e3996be
Ridwan <aladebaruwa@gmail.com>
2024-07-28 20:01:53 +0100

    Added JavaScript code for snake game logic, updated CSS styles for game elements, and modified HTML to include a logo image with an ID

---
+++ assets/scripts/script.js
@@ -0,0 +1,43 @@
+// Define HTML elements
+
+const board = document.getElementById('game-board');
+
+// Define game variables
+
+let snake = [{x: 10, y: 10}];
+
+// Draw game map, snake, food
+
+function draw() {
+    board.innerHTML = '';
+    drawSnake();
+}
+
+// Draw Snake
+
+function drawSnake() {
+    snake.forEach((segment) => {
+        const snakeElement = createGameElement('div', 'snake')
+    });
+    SetPosition(snakeElement, segment);
+    board.appendChild(snakeElement)
+}
+
+// Create a snake or food cube/div
+
+function createGameElement(tag, className) {
+    const element = document.createElement(tag);
+    element.className = className;
+    return element;
+}
+
+//set the position of snake or food
+
+function SetPosition(element, position) {
+    element.style.gridColumn = position.x;
+    element.style.gridRow = position.y;
+}
+
+
+// Testing draw function
+draw();
